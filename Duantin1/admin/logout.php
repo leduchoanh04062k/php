1df55188a84571da2415472e7dda4f89a93c7005
leduchoanh04062k <68231217+leduchoanh04062k@users.noreply.github.com>
2020-07-16 23:49:06 +0700

    l
    up

--- Duantin1/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-	require_once '../commons/constants.php';
-    require_once '../commons/db.php';
-    require_once '../commons/helpers.php';
-    unset($_SESSION[name]);
-    header('location: ' . BASE_URL);
-    
- ?>
